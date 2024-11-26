2c4bd083cca1f5b435188fade14d54c74d879cc9
HTH016 <hthth01116@gmail.com>
2024-11-27 07:02:26 +0900

    init commit

---
+++ src/main/java/flab/Linkedlog/LinkedLogApplication.java
@@ -0,0 +1,13 @@
+package flab.Linkedlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flab/Linkedlog/LinkedLogApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.Linkedlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
